b8ac7be40302543f56eae05b96180c2cb964bd02
EffeminateFox <111376341+EffeminateFox@users.noreply.github.com>
2024-07-02 22:34:32 -0400

    Create interestRate.js

---
+++ interestRate.js
@@ -0,0 +1,7 @@
+// U54529624
+
+// Function to calculate interest
+// Formula: Interest = Principal * Rate * Time
+export function calculateInterest(principal, rate, time) {
+    return principal * rate * time;
+}
